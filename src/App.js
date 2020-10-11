27f861900c90be7557731abc22feeabdd1c4f8d3
Ksun <ksy03070@gmail.com>
2020-10-11 15:42:39 +0000

    Generate Dynamit Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
-function Food({favorite}){
+function Food({name, image}){
 	return(
-		<h3>I love {favorite}</h3>
+		<div>
+			<h3>I love {name}</h3>
+			<img src={image} alt="foodImage" />
+		</div>
 	);
 }
+
+const foodILike = [
+	{
+		name: "kimchi",
+		image: "kimchiImage"
+	},
+	{
+		name: "ramen",
+		image: "ramenImage"
+	},
+	{
+		name: "chukumi",
+		image: "chukumiImage"
+	}
+]
 
 function App() {
   return (
     <div>
-		  <h1>Hello</h1>
-		  <Food favorite="kimchi" />
-		  <Food favorite="ramen" />
-		  <Food favorite="chukumi" />
+		  <h1>Food Factory</h1>
+		  {foodILike.map(dish => (
+			  <Food key={dish.name} name={dish.name} image={dish.image} />
+		  ))}
 	</div>
   );
 }
