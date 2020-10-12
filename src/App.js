f1a551e2927b55e186865e39d04e902dff8008ab
Ksun <ksy03070@gmail.com>
2020-10-12 02:15:54 +0000

    Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({name, image}){
+const foodILike = [
+	{
+		id: 1,
+		name: "kimchi",
+		image: "kimchiImage",
+		rating: 5
+	},
+	{
+		id: 2,
+		name: "ramen",
+		image: "ramenImage",
+		rating: 4.9
+	},
+	{
+		id: 3,
+		name: "chukumi",
+		image: "chukumiImage",
+		rating: 3.8
+	}
+]
+
+function Food({name, picture, rating}){
 	return(
 		<div>
 			<h3>I love {name}</h3>
-			<img src={image} alt="foodImage" />
+			<h4>{rating}/5.0</h4>
+			<img src={picture} alt="foodImage" />
 		</div>
 	);
 }
 
-const foodILike = [
-	{
-		name: "kimchi",
-		image: "kimchiImage"
-	},
-	{
-		name: "ramen",
-		image: "ramenImage"
-	},
-	{
-		name: "chukumi",
-		image: "chukumiImage"
-	}
-]
+Food.propTypes = {
+	name: PropTypes.string.isRequired,
+	picture: PropTypes.string.isRequired,
+	rating: PropTypes.number.isRequired
+}
 
 function App() {
   return (
     <div>
 		  <h1>Food Factory</h1>
 		  {foodILike.map(dish => (
-			  <Food key={dish.name} name={dish.name} image={dish.image} />
+			  <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
 		  ))}
 	</div>
   );
